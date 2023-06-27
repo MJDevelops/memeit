c2be42984aa4115a2ef74bc85695f9fe4dde01a1
MJProgramming <joey.meierkord@gmail.com>
2023-06-27 16:37:35 +0200

    Add link tags for used font to eliminate warning by styled components

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Bitter:wght@400;700;900&display=swap');
-  
   body {
     font-family: 'Bitter', sans-serif;
   }
