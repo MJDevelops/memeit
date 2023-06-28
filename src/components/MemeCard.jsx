98bef02c75b1802ad5eae9bfa88383aecf2e92d0
MJProgramming <joey.meierkord@gmail.com>
2023-06-28 23:36:51 +0200

    Add styled div for layer effect

--- src/components/MemeCard.jsx
+++ src/components/MemeCard.jsx
@@ -5,12 +5,14 @@ const MemeCard = ({ image }) => {
   return (
     <CardWrapper>
       <img style={{width: '100%', height: '100%'}} src={image} />
+      <LayerEffect />
     </CardWrapper>
   );
 };
 
 const CardWrapper = styled.div`
   display: flex;
+  position: relative;
   width: 60%;
   height: 60%;
   flex-direction: column;
@@ -18,6 +20,24 @@ const CardWrapper = styled.div`
   background-color: gray;
   align-items: center;
   justify-content: center;
+`;
+
+const LayerEffect = styled.div`
+  display: flex;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.5);
+
+    &::before {
+      color: white;
+      content: "Choose";
+    }
+  }
 `;
 
 MemeCard.propTypes = {
