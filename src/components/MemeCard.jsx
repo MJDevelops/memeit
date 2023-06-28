d2d6fbcd8918ac870032811b8de327edf35e2cf5
MJProgramming <joey.meierkord@gmail.com>
2023-06-28 20:44:18 +0200

    Change size of styled component

--- src/components/MemeCard.jsx
+++ src/components/MemeCard.jsx
@@ -4,16 +4,15 @@ import PropTypes from 'prop-types';
 const MemeCard = ({ image }) => {
   return (
     <CardWrapper>
-      <img style={{width: '100%', height: 'auto'}} src={image} />
+      <img style={{width: '100%', height: '100%'}} src={image} />
     </CardWrapper>
   );
 };
 
 const CardWrapper = styled.div`
   display: flex;
-  width: 50%;
-  height: 50%;
-  border-radius: 50px;
+  width: 60%;
+  height: 60%;
   flex-direction: column;
   border: 2px solid black;
   background-color: gray;
