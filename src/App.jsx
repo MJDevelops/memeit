95012f5567198e31616390826dcc690bce66d43b
MJProgramming <joey.meierkord@gmail.com>
2023-06-28 20:45:07 +0200

    Add eslint json file, change behavior of overflow property in App

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,8 @@ const AppWrapper = styled.div`
   flex-direction: column;
   width: 100vw;
   height: 100vh;
-  overflow: hidden;
+  overflow-y: auto;
+  overflow-x: hidden;
 `;
 
 export default App;
