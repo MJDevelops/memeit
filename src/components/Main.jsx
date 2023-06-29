34ed5b286ae0a0e12a000ff82b162962eddface3
MJProgramming <joey.meierkord@gmail.com>
2023-06-29 03:16:40 +0200

    Various changes for checking if the game is over
    The useEffect hook was changed to consider if the game is over
    to not fetch more memes if the new round did not yet start.
    Further functions were added to handle the clickedMemes state cleaner.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,24 +1,46 @@
 import styled from 'styled-components';
 import CardGrid from './CardGrid';
+import GameOver from './GameOver';
 import { shuffleArray } from '../utils';
 import { useState, useEffect } from 'react';
 
 const Main = () => {
   const [memes, setMemes] = useState([]);
   const [clickedMemes, setClickedMemes] = useState([]);
+  const [isGameOver, setIsGameOver] = useState(false);
+
+  const resetMemes = () => {
+    setMemes([]);
+  };
+
+  const resetClickedMemes = () => {
+    setClickedMemes([]);
+  };
+
+  const handleClickedMeme = (id) => {
+    if (clickedMemes.includes(id)) {
+      resetMemes();
+      resetClickedMemes();
+      setIsGameOver(true);
+    } else {
+      setClickedMemes([...clickedMemes, id])
+    }
+  };
 
   useEffect(() => {
-    const fetchMemes = async () => {
-      const memes = await fetch('https://meme-api.com/gimme/dankmemes/8');
-      const memesJSON = await memes.json();
+    if (memes.length === 0 && !isGameOver) {
+      const fetchMemes = async () => {
+        const memes = await fetch('https://meme-api.com/gimme/dankmemes/8');
+        const memesJSON = await memes.json();
 
-      return memesJSON['memes'].map((meme, index) => (
-        { id: index, memeURL: meme.url }
-      ));
-    };
+        return memesJSON['memes'].map((meme, index) => (
+          { id: index, memeURL: meme.url }
+        ));
+      };
 
-    fetchMemes().then((arr) => setMemes(arr));
-  }, []);
+      fetchMemes().then((arr) => setMemes(arr));
+    }
+  }, [memes, isGameOver]);
 
   useEffect(() => {
     setMemes(shuffleArray(memes));
@@ -26,11 +48,15 @@ const Main = () => {
 
   return (
     <StyledMain>
-      <CardGrid
-        clickedMemesArr={clickedMemes}
-        memesArr={memes}
-        handleClickedMeme={setClickedMemes}
-      />
+      {
+        isGameOver 
+        ? <GameOver handleGameOver={setIsGameOver} /> 
+        : <CardGrid
+            clickedMemesArr={clickedMemes}
+            memesArr={memes}
+            handleClickedMeme={handleClickedMeme}
+        />
+      }
     </StyledMain>
   );
 }
